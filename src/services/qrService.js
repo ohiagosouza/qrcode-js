b17cb248da1c90f0f5ee51a8471925c0fe1a9054
Hiago Souza <ohiagossouza@gmail.com>
2024-04-25 02:49:40 -0300

    change path to save in a local folder & add regex QR CODE filename

--- src/services/qrService.js
+++ src/services/qrService.js
@@ -1,9 +1,14 @@
 const QRCode = require("qrcode");
+const fs = require("node:fs");
+
+const save_path = fs.realpathSync("qrcode-png", "utf-8");
 
 const qrService = {
   generateQRCode: async data => {
+    const fileName = data.replace(/[/.]/g, "_");
+
     try {
-      const qrCode = await QRCode.toFile(`../../Downloads/qr-${data}`, data, {
+      const qrCode = await QRCode.toFile(`${save_path}/${fileName}_qrcode`, data, {
         errorCorrectionLevel: "H",
         type: "png",
         color: { dark: "FFF", light: "000" },
